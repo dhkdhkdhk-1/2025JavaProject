8508008b0b253acb644df8a7767d8fcc3ff9fcd0
DESKTOP-09CKEV3\dlwlghks <wlghksdhxk1513!>
2025-10-14 14:45:51 +0900

    1

--- backend/src/main/java/kr/ac/ync/library/domain/auth/controller/AuthController.java
+++ backend/src/main/java/kr/ac/ync/library/domain/auth/controller/AuthController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173"})
 public class AuthController
 {
 
