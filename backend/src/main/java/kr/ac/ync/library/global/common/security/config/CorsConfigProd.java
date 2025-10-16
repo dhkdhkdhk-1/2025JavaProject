035c2308ca829b50e9d4d70d316352a9a3213437
dhkdhkdhk <3160rnjs@naver.com>
2025-10-16 16:11:06 +0900

    backend test

--- backend/src/main/java/kr/ac/ync/library/global/common/security/config/CorsConfigProd.java
+++ backend/src/main/java/kr/ac/ync/library/global/common/security/config/CorsConfigProd.java
@@ -1,4 +1,4 @@
-package kr.ac.ync.library.global.config;
+package kr.ac.ync.library.global.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
