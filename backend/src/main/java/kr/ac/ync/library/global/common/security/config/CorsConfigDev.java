52050086df89926f5e9429c6a57c8a6b845c7246
dhkdhkdhk <3160rnjs@naver.com>
2025-10-16 15:21:26 +0900

    Backend front 연동 test

---
+++ backend/src/main/java/kr/ac/ync/library/global/common/security/config/CorsConfigDev.java
@@ -0,0 +1,26 @@
+package kr.ac.ync.library.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@Profile("dev") // ✅ 개발 환경에서만 활성화
+public class CorsConfigDev {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("http://localhost:3000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
