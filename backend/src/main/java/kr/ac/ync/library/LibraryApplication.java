4beed4e5031491263fd8b22894795db8229f158e
dhkdhkdhk <3160rnjs@naver.com>
2025-10-16 12:49:27 +0900

    Backend deploy test

--- backend/src/main/java/kr/ac/ync/library/LibraryApplication.java
+++ backend/src/main/java/kr/ac/ync/library/LibraryApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class LibraryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("asd");
         SpringApplication.run(LibraryApplication.class, args);
 	}
 
