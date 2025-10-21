e78d9e7fdba2058e814540b32ba001ac9cc08121
kkahwan <doong7871@naver.com>
2025-10-21 15:41:22 +0900

    no message

---
+++ backend/src/main/generated/kr/ac/ync/library/domain/email/entity/QEmailLogEntity.java
@@ -0,0 +1,45 @@
+package kr.ac.ync.library.domain.email.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QEmailLogEntity is a Querydsl query type for EmailLogEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QEmailLogEntity extends EntityPathBase<EmailLogEntity> {
+
+    private static final long serialVersionUID = 1034689842L;
+
+    public static final QEmailLogEntity emailLogEntity = new QEmailLogEntity("emailLogEntity");
+
+    public final StringPath content = createString("content");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath recipient = createString("recipient");
+
+    public final DateTimePath<java.time.LocalDateTime> sentAt = createDateTime("sentAt", java.time.LocalDateTime.class);
+
+    public final StringPath subject = createString("subject");
+
+    public QEmailLogEntity(String variable) {
+        super(EmailLogEntity.class, forVariable(variable));
+    }
+
+    public QEmailLogEntity(Path<? extends EmailLogEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QEmailLogEntity(PathMetadata metadata) {
+        super(EmailLogEntity.class, metadata);
+    }
+
+}
+
