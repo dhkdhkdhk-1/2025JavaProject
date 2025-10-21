3a2a9750afdb083593cdd9ca9b5d4c26328d4c34
dhkdhkdhk <3160rnjs@naver.com>
2025-10-21 12:43:09 +0900

    deploy test 8

--- backend/src/test/java/kr/ac/ync/library/domain/users/repository/UserRepositoryTest.java
+++ backend/src/test/java/kr/ac/ync/library/domain/users/repository/UserRepositoryTest.java
@@ -21,35 +21,4 @@ class UserRepositoryTest
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @Test
-    void testInertUsers()
-    {
-        UserEntity user = UserEntity.builder().email("rnjsehsgus31@gmail.com").password(passwordEncoder.encode("1231")).role(UserRole.USER).username("이메일").build();
-        userRepository.save(user);
-        IntStream.rangeClosed(1, 100)
-                .forEach(i -> {
-                    String email = "user" + i + "@aaa.com";
-                    UserEntity userEntity = UserEntity.builder()
-                            .email(email)
-                            .password(passwordEncoder.encode("1111"))
-                            .username("USER" + i)
-                            .role(UserRole.USER)
-                            .build();
-                    userRepository.save(userEntity);
-                });
-    }
-
-    @Test
-    void testUpdateMember()
-    {
-        userRepository.findById(100L)
-                .map(UserEntity ->
-                {
-                    User user = UserMapper.toDTO(UserEntity);
-                    user.setRole(UserRole.ADMIN);
-                    userRepository.save(UserMapper.toEntity(user));
-                    return true;
-                });
-    }
-
 }
