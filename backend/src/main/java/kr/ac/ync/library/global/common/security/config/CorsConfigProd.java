720e2332469a6a3a15991f30f001c3bda997c86d
dhkdhkdhk <3160rnjs@naver.com>
2025-10-21 13:05:56 +0900

    deploy test 10

--- backend/src/main/java/kr/ac/ync/library/global/common/security/config/CorsConfigProd.java
+++ backend/src/main/java/kr/ac/ync/library/global/common/security/config/CorsConfigProd.java
@@ -7,6 +7,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 @Profile("prod") // ✅ 운영 환경에서만 활성화
 public class CorsConfigProd {
@@ -14,12 +16,17 @@ public class CorsConfigProd {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOriginPattern("https://d577bst0lphds.cloudfront.net");
-        config.addAllowedOriginPattern("http://ync-library-front.s3-website-ap-southeast-2.amazonaws.com");
-        config.addAllowedOriginPattern("https://13.239.18.193");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+
+        // ✅ CloudFront, EC2 둘 다 허용
+        config.setAllowedOrigins(Arrays.asList(
+                "https://d577bst0lphds.cloudfront.net",
+                "https://13.239.18.193"
+        ));
+
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
