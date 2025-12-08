0b87251acabb6cbb0e3488f1e8e8893440fdf518
kkahwan <doong7871@naver.com>
2025-12-08 14:07:12 +0900

    2025/12/08 cs 구현중 유저 개인 문의 목록 리스트 조회 구현중

--- backend/src/main/java/kr/ac/ync/library/domain/cs/controller/CsController.java
+++ backend/src/main/java/kr/ac/ync/library/domain/cs/controller/CsController.java
@@ -1,9 +1,17 @@
 package kr.ac.ync.library.domain.cs.controller;
 
 import jakarta.validation.Valid;
+import kr.ac.ync.library.domain.cs.dto.CsListResponse;
 import kr.ac.ync.library.domain.cs.dto.CsRegisterRequest;
+import kr.ac.ync.library.domain.cs.entity.CsEntity;
 import kr.ac.ync.library.domain.cs.service.CsService;
+import kr.ac.ync.library.domain.users.dto.User;
+import kr.ac.ync.library.domain.users.service.UserService;
+import kr.ac.ync.library.global.common.security.UserSecurity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +21,21 @@ import org.springframework.web.bind.annotation.*;
 public class CsController {
 
     private final CsService csService;
+    private final UserSecurity userSecurity;
+    private final UserService userService;
 
-    @PostMapping("/write")
-    public ResponseEntity<CsRegisterRequest> write(
-            @Valid @RequestBody CsRegisterRequest csRegisterRequest )
+    @PostMapping
+    public ResponseEntity register(
+            @Valid @RequestBody CsRegisterRequest csRegisterRequest)
     {
-//         csService.register(csRegisterRequest, branchId);
-        return  null;
+        csService.register(csRegisterRequest);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/list/me")
+    public ResponseEntity getMyCsList() {
+        User user = userSecurity.getUser();
+        Page<CsListResponse> myList = csService.getMyList(user.getId(), Pageable.ofSize(10));
+        return ResponseEntity.ok(myList);
     }
 }
